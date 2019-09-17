bc8a6da6134f51627200521b392a4425df380669
Paul Saada <paulsaada6@gmail.com>
2019-09-17 15:06:36 -0400

    added email container file path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Title from './components/Title';
+import Title from './components/containers/Title Container/Title';
 import Subtitle from './components/Subtitle';
-import EmailField from './components/EmailField';
-import SubmitButton from './components/SubmitButton';
+import EmailField from './components/containers/Form Container/Email Container/EmailField';
+import SubmitButton from './components/containers/Form Container/Email Container/SubmitButton';
 import UserAgreement from './components/containers/Form Container/Privacy Container/UserAgreement';
 import Checkbox from './components/containers/Form Container/Privacy Container/Checkbox';
 
