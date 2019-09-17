47681aca43c860c7ca86a76ba12adf40da335280
Paul Saada <paulsaada6@gmail.com>
2019-09-17 16:02:30 -0400

    removed more uneccesary folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import Title from './components/containers/Title Container/Title';
-import Subtitle from './components/containers/Form Container/Privacy Container/Subtitle';
-import EmailField from './components/containers/Form Container/Email Container/EmailField';
-import SubmitButton from './components/containers/Form Container/Email Container/SubmitButton';
-import UserAgreement from './components/containers/Form Container/Privacy Container/UserAgreement';
-import Checkbox from './components/containers/Form Container/Privacy Container/Checkbox';
+import Subtitle from './components/containers/Privacy Container/Subtitle';
+import EmailField from './components/containers/Email Container/EmailField';
+import SubmitButton from './components/containers/Email Container/SubmitButton';
+import UserAgreement from './components/containers/Privacy Container/UserAgreement';
+import Checkbox from './components/containers/Privacy Container/Checkbox';
 
 class App extends React.Component {
    state = {}
