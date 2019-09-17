4c001a62b6b2dcfb8129424d20757cd9a81f4f14
Paul Saada <paulsaada6@gmail.com>
2019-09-17 15:09:52 -0400

    updated file path

---
+++ src/components/containers/Form Container/Email Container/SubmitButton.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './SubmitButton.css';
+
+class SubmitButton extends Component {
+   state = {}
+   render() {
+      return (
+         <div>
+            <button className="submitBtn">NEXT</button>
+         </div>
+      );
+   }
+}
+
+export default SubmitButton;
---
+++ src/components/containers/Title Container/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+const Title = () => {
+	return (
+		<div>
+			<h1 className="title">join the list</h1>
+		</div>
+	);
+};
+
+export default Title;
