e0a9931ef5ff7a29bf5d7af50338d425e74636b7
Paul Saada <paulsaada6@gmail.com>
2019-09-17 10:40:28 -0400

    added subtitle component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import './App.css';
+import Title from './components/Title';
+import Subtitle from './components/Subtitle'
 
+class App extends React.Component {
+   state = {}
+   render() {
+      return (
+         <div>
+            <Title />
+            <Subtitle />
+         </div>
+      );
+   }
+}
 
 export default App;
