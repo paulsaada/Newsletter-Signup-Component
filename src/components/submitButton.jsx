ecc67f29a794ce7c2f3bd8a930a6f83dfdeb5b37
Paul Saada <paulsaada6@gmail.com>
2019-09-17 11:25:06 -0400

    added submit button

---
+++ src/components/submitButton.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class submitButton extends Component {
+   state = {}
+   render() {
+      return (
+         <div>
+            <button>NEXT</button>
+         </div>
+      );
+   }
+}
+
+export default submitButton;
--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './title.css';
+import './Title.css';
 
 const Title = () => {
 	return (
