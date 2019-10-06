3c64187d699441c8e15b5e2b51ea0fcea9c7c015
Paul Saada <paulsaada6@gmail.com>
2019-10-06 16:39:07 -0400

    added congratulations page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import Title from './components/title/title';
-// import UserEmailCollection from './pages/user-email-collection';
 import UserNameCollection from './pages/user-name-collection';
+import UserEmailCollection from './pages/user-email-collection';
+import CongratsUser from './pages/congratulations';
 
 class App extends React.Component {
    state = {}
@@ -10,7 +11,9 @@ class App extends React.Component {
       return (
          <div className="appComponent">
             <Title />
+            <UserEmailCollection />
             <UserNameCollection />
+            <CongratsUser />
          </div>
       );
    }
