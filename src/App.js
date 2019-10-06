31803a471551fa9fb16e1fc16aaf99d7c913d9cf
Paul Saada <paulsaada6@gmail.com>
2019-10-06 10:58:06 -0400

    added pages directory

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Title from './components/title/title';
+import Form from './containers/form/form'
+
+class App extends React.Component {
+   state = {}
+   render() {
+      return (
+         <div className="appComponent">
+            <Title />
+            <Form />
+         </div>
+      );
+   }
+}
+
+export default App;
