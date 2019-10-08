6f9232c2546ee0f51aacd76ce8a31773126c9c77
Paul Saada <paulsaada6@gmail.com>
2019-10-08 08:07:57 -0400

    added switch statement

--- src/App.js
+++ src/App.js
@@ -14,17 +14,50 @@ class App extends React.Component {
    }
 
    // proceed to next step
+   nextStep = () => {
+      const { step } = this.state;
+      this.setState({
+         step: step + 1
+      });
+   }
 
+   // handle change upon user input
+   handleChange = input => e => {
+      this.setState({ [input]: e.target.value });
+   }
 
    render() {
-      return (
-         <div className="appComponent">
-            <Title />
-            <UserEmailCollection />
-            <UserNameCollection />
-            <CongratsUser />
-         </div>
-      );
+      const { step } = this.state;
+      const { email, firstName, lastName } = this.state;
+      const values = { email, firstName, lastName };
+
+      switch (step) {
+         case 1:
+            return (
+               <div className="appComponent">
+                  <Title />
+                  <UserEmailCollection />
+               </div>
+            )
+
+         case 2:
+            return (
+               <div className="appComponent">
+                  <Title />
+                  <UserNameCollection />
+               </div>
+            )
+
+         case 3:
+            return (
+               <div className="appComponent">
+                  <Title />
+                  <CongratsUser />
+               </div>
+            )
+         default:
+         // do nothing
+      }
    }
 }
 
